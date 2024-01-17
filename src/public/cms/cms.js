32757b7cc5f62db854a8ca08b0188f81dcaee8d8
FalseForest <maxim.dumont2000@hotmail.com>
2024-01-17 19:20:33 +0100

    remove cms.js

--- src/public/cms/cms.js
+++
@@ -1,7 +0,0 @@
-import CMS from 'decap-cms-app';
-import {
-    BehindTheScenesWidgetControl,
-    BehindTheScenesWidgetPreview
-} from "../components/custom_widgets/BehindTheScenesWidget";
-
-CMS.registerWidget('BTS', BehindTheScenesWidgetControl, BehindTheScenesWidgetPreview);
